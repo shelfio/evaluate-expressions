83832da6723397dbc8f1c200a87475b0e0cddb9c
DrewsAbuse <drewsdrews1999@gmail.com>
2023-02-17 01:18:47 +0100

    test: Added case 'should return `true`'

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,63 @@
-import {getFoo} from './index';
+import type {Expression} from './index';
+import {evaluateExpression} from './index';
 
-it('should return bar', () => {
-  expect(getFoo()).toEqual('bar');
+describe('evaluateExpression', () => {
+  it('should return `true`', () => {
+    const variablesWithValue = [
+      {
+        id: 'variable-id-a',
+        value: 'a',
+      },
+      {
+        id: 'variable-id-b',
+        value: 'b',
+      },
+      {
+        id: 'variable-id-c',
+        value: 'c',
+      },
+      {
+        id: 'variable-id-d',
+        value: 'd',
+      },
+    ];
+
+    const expression: Expression = {
+      joiner: 'AND',
+      rules: [
+        {
+          joiner: 'OR',
+          rules: [
+            {
+              variableId: 'variable-id-c',
+              operator: 'eq',
+              value: 'c',
+            },
+            {
+              variableId: 'variable-id-c',
+              operator: 'eq',
+              value: 'c',
+            },
+            {
+              variableId: 'variable-id-d',
+              operator: 'eq',
+              value: 'd',
+            },
+          ],
+        },
+        {
+          variableId: 'variable-id-a',
+          operator: 'eq',
+          value: 'a',
+        },
+        {
+          variableId: 'variable-id-b',
+          operator: 'eq',
+          value: 'b',
+        },
+      ],
+    };
+
+    expect(evaluateExpression(expression, variablesWithValue)).toEqual(true);
+  });
 });
