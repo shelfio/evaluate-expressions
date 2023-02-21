88e72d2a0afc20a6773b7a1aceca466184e3e5b7
DrewsAbuse <drewsdrews1999@gmail.com>
2023-02-21 23:28:21 +0100

    feat: Added main `evaluateExpression` function

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,240 +1,38 @@
-import type {Expression} from './index';
+jest.mock('./evaluation');
+
+import {expressionToRPN} from './evaluation';
 import {evaluateExpression} from './index';
 
-const baseExpression: Expression = {
-  joiner: 'AND',
-  rules: [
-    {
-      joiner: 'OR',
-      rules: [
-        {
-          variableId: 'variable-id-c',
-          operator: 'eq',
-          value: 'c',
-        },
-        {
-          variableId: 'variable-id-b',
-          operator: 'eq',
-          value: 'b',
-        },
-      ],
-    },
-    {
-      variableId: 'variable-id-a',
-      operator: 'eq',
-      value: 'a',
-    },
-  ],
-};
+it('should return true', () => {
+  jest.mocked(expressionToRPN).mockReturnValue([true]);
 
-const notBinaryExpression: Expression = {
-  joiner: 'AND',
-  rules: [
-    {
-      joiner: 'OR',
-      rules: [
-        {
-          variableId: 'variable-id-c',
-          operator: 'eq',
-          value: 'c',
-        },
-        {
-          variableId: 'variable-id-b',
-          operator: 'eq',
-          value: 'b',
-        },
-        {
-          variableId: 'variable-id-d',
-          operator: 'eq',
-          value: 'd',
-        },
-      ],
-    },
-    {
-      variableId: 'variable-id-e',
-      operator: 'neq',
-      value: 'e',
-    },
-    {
-      variableId: 'variable-id-a',
-      operator: 'eq',
-      value: 'a',
-    },
-  ],
-};
-
-const singleRuleExpression: Expression = {
-  rules: [
-    {
-      variableId: 'variable-id-a',
-      operator: 'eq',
-      value: 'a',
-    },
-  ],
-};
-
-const variablesWithValue = [
-  {
-    id: 'variable-id-a',
-    value: 'a',
-  },
-  {
-    id: 'variable-id-b',
-    value: 'b',
-  },
-  {
-    id: 'variable-id-c',
-    value: 'wrong-value',
-  },
-  {
-    id: 'variable-id-d',
-    value: 'wrong-value',
-  },
-  {
-    id: 'variable-id-e',
-    value: 'wrong-value',
-  },
-];
-
-describe('evaluateExpression', () => {
-  it('should return `true`', () => {
-    expect(evaluateExpression(baseExpression, variablesWithValue)).toEqual(true);
-  });
-
-  it('should return `true` for expression w/ non-binary operator', () => {
-    expect(evaluateExpression(notBinaryExpression, variablesWithValue)).toEqual(true);
-  });
-
-  it('should return `true` for single rule when passed correct variable value', () => {
-    expect(evaluateExpression(singleRuleExpression, variablesWithValue)).toEqual(true);
-  });
-
-  it('should return `false` for single rule when passed wrong variable value', () => {
-    const variablesWithValue = [
+  const expression = {
+    rules: [
       {
-        id: 'variable-id-a',
-        value: 'wrong-value',
+        variableId: 'variable-id-a',
+        operator: 'contains' as const,
+        value: 'some',
       },
-    ];
+    ],
+  };
 
-    const expression: Expression = {
-      rules: [
-        {
-          variableId: 'variable-id-a',
-          operator: 'eq',
-          value: 'a',
-        },
-      ],
-    };
+  expect(evaluateExpression(expression, [{id: 'variable-id-a', value: 'something'}])).toBe(true);
+});
 
-    expect(evaluateExpression(expression, variablesWithValue)).toEqual(false);
-  });
+it('should throw `Invalid expression result` if expressionToRPN return undefined', () => {
+  jest.mocked(expressionToRPN).mockReturnValue([undefined as any]);
 
-  it('should return `true` for complex expression', () => {
-    const complexExpression: Expression = {
-      joiner: 'AND',
-      rules: [
-        baseExpression,
-        {
-          joiner: 'OR',
-          rules: [
-            notBinaryExpression,
-            {
-              rules: [
-                {
-                  variableId: 'variable-id-a',
-                  operator: 'eq',
-                  value: 'wrong-value',
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    };
-
-    expect(evaluateExpression(complexExpression, variablesWithValue)).toEqual(true);
-  });
-
-  it('should return `false` for complex expression', () => {
-    const complexExpression: Expression = {
-      joiner: 'AND',
-      rules: [
-        baseExpression,
-        {
-          joiner: 'AND',
-          rules: [
-            notBinaryExpression,
-            {
-              rules: [
-                {
-                  variableId: 'variable-id-a',
-                  operator: 'eq',
-                  value: 'wrong-value',
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    };
-
-    expect(evaluateExpression(complexExpression, variablesWithValue)).toEqual(false);
-  });
-
-  it('should throw error if passed incorrect expression w/ joiner `AND`', () => {
-    expect.assertions(1);
-
-    const variablesWithValue = [
+  const expression = {
+    rules: [
       {
-        id: 'variable-id-a',
-        value: 'a',
+        variableId: 'variable-id-a',
+        operator: 'contains' as const,
+        value: 'some',
       },
-    ];
+    ],
+  };
 
-    const expression: Expression = {
-      joiner: 'AND',
-      rules: [
-        {
-          variableId: 'variable-id-a',
-          operator: 'eq',
-          value: 'a',
-        },
-      ],
-    };
-
-    try {
-      evaluateExpression(expression, variablesWithValue);
-    } catch ({message}) {
-      expect(message).toEqual('Invalid expression with joiner AND, right side is missing');
-    }
-  });
-
-  it('should throw error if passed incorrect expression w/ joiner `OR`', () => {
-    expect.assertions(1);
-
-    const variablesWithValue = [
-      {
-        id: 'variable-id-a',
-        value: 'a',
-      },
-    ];
-
-    const expression: Expression = {
-      joiner: 'OR',
-      rules: [
-        {
-          variableId: 'variable-id-a',
-          operator: 'eq',
-          value: 'a',
-        },
-      ],
-    };
-
-    try {
-      evaluateExpression(expression, variablesWithValue);
-    } catch ({message}) {
-      expect(message).toEqual('Invalid expression with joiner OR, right side is missing');
-    }
-  });
+  expect(() => evaluateExpression(expression, [{id: 'variable-id-a', value: 'something'}])).toThrow(
+    'Invalid expression result'
+  );
 });
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,5 @@
-export type Rule = {
-  variableId: string;
-  operator: 'eq' | 'neq';
-  value: string;
-};
-export type Expression = {
-  joiner?: 'OR' | 'AND';
-  rules: (Expression | Rule)[];
-};
-export type VariableWithValue = {id: string; value: string};
-
-type StackElement = boolean | 'OR' | 'AND';
-type JoinerParameters = {left: StackElement; right: StackElement};
-
-const invokeJoinerMap = {
-  OR: ({left, right}: JoinerParameters) => !!(left || right),
-  AND: ({left, right}: JoinerParameters) => !!(left && right),
-  default: ({left}: JoinerParameters) => !!left,
-};
-
-type RuleParameters = {passedValue: string | undefined; comparedValue: string};
-const invokeRuleMap = {
-  eq: ({passedValue, comparedValue}: RuleParameters) => passedValue === comparedValue,
-  neq: ({passedValue, comparedValue}: RuleParameters) => passedValue !== comparedValue,
-};
+import type {Expression, VariableWithValue} from './types';
+import {expressionToRPN} from './evaluation';
 
 export const evaluateExpression = (
   expression: Expression,
@@ -33,55 +10,10 @@ export const evaluateExpression = (
   const [result] = expressionToRPN(expression, variableIdToVariablesMap);
 
   if (typeof result !== 'boolean') {
-    throw new Error('Invalid expression');
+    throw new Error('Invalid expression result', {
+      cause: 'invalid-expression',
+    });
   }
 
   return result;
 };
-const expressionToRPN = (
-  expression: Expression | Rule,
-  variableIdToValuesMap: Map<string, string>
-): StackElement[] => {
-  if ('rules' in expression) {
-    const rpn = transformToBinaryOperators(expression).rules.flatMap(rule =>
-      expressionToRPN(rule, variableIdToValuesMap)
-    );
-
-    const [left, right] = rpn.splice(-2, 2);
-
-    if (expression.joiner && right === undefined) {
-      throw new Error(`Invalid expression with joiner ${expression.joiner}, right side is missing`);
-    }
-
-    rpn.push(invokeJoinerMap[expression.joiner ?? 'default']({left, right}));
-
-    return rpn;
-  }
-
-  const passedValue = variableIdToValuesMap.get(expression.variableId);
-  const comparedValue = expression.value;
-
-  return [
-    invokeRuleMap[expression.operator]({
-      passedValue,
-      comparedValue,
-    }),
-  ];
-};
-
-function transformToBinaryOperators(expression: Expression): Expression {
-  if (expression.rules.length <= 2) {
-    return expression;
-  }
-
-  const [firstRule, ...restOfRules] = expression.rules;
-  const equivalentExpression = transformToBinaryOperators({
-    joiner: expression.joiner,
-    rules: restOfRules,
-  });
-
-  return {
-    joiner: expression.joiner,
-    rules: [firstRule, equivalentExpression],
-  };
-}
